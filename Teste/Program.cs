89fee9dc60099ac18c3a360c4df4a867d2cd571d
maikeliani <maikel.iani31@gmail.com>
2023-05-04 15:52:22 -0300

    comentarios e tste de impressão em console

---
+++ Teste/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Net;
+using System.Net.Http.Json;
+using System.Security.Cryptography.X509Certificates;
+using ProjRabbitMQ.Models;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        HttpClient rabbit = new HttpClient();
+        Message mensage = new()
+        {
+            Description = " to com fome"
+        };
+
+        for (int i = 0; i < 10000; i++)
+        {
+
+            HttpResponseMessage response = rabbit.PostAsJsonAsync("https://localhost:7195/api/Messages", mensage).Result;
+            //como nao esta em una funcao async e esta dando um post async então precisa do .Result!!
+            //A Consumer vai automaticamente ( a cada 2 segundos) visitando a fila e no caso desse teste ele vai imprimir as mensagens
+            //atraves do comando  --> Console.WriteLine("Description: " + message.Description);  que esta dentro da Consumer
+        }
+    }
+}
